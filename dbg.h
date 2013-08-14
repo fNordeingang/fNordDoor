895bcfc8f61d4dfee298b09fe3db72f5ed8597c1
vileda <vileda@piratenfraktion-nrw.de>
2013-08-14 16:25:02 +0200

    added time to logger

--- dbg.h
+++ dbg.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
 
 #ifdef NDEBUG
 #define debug(M, ...)
@@ -11,13 +12,25 @@
 #define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
+static char* now() {
+  time_t     now = time(0);
+  struct tm  tstruct;
+  char       *buf = (char*)malloc(80*sizeof(char));
+  tstruct = *localtime(&now);
+  strftime(buf, 80*sizeof(char)-1, "%F %R:%S", &tstruct);
+
+  return buf;
+}
+
+static char* _now;
+
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) _now = now();fprintf(stderr, "[ERROR] %s (%s:%d: errno: %s) " M "\n", _now, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__); free(_now)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) _now = now();fprintf(stderr, "[WARN] %s (%s:%d: errno: %s) " M "\n", _now, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__); free(_now)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) _now = now();fprintf(stderr, "[INFO] %s (%s:%d) " M "\n", _now, __FILE__, __LINE__, ##__VA_ARGS__); free(_now)
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
